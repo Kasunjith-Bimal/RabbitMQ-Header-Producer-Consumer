02ffea080323bdc7a24f0e8b3754158f321e39e0
Kasunjith-Bimal <kasunysoft@gmail.com>
2022-02-28 21:34:30 +0530

    header exchange

---
+++ RabitMQ.Producer/HeaderExchangeProducer.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabitMQ.Producer
+{
+    public static class HeaderExchangeProducer
+    {
+        public static void Publish(IModel channel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-messagee-ttl",30000 }
+            };
+            channel.ExchangeDeclare("demo-header-exchange", ExchangeType.Headers, arguments: ttl);
+
+            var count = 0;
+            while (true)
+            {
+                var message = new { name = "product", Message = $"Hello! Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                var properties =  channel.CreateBasicProperties();
+                properties.Headers = new Dictionary<string, object> { { "account", "new" } };
+                channel.BasicPublish("demo-header-exchange", string.Empty, properties, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
